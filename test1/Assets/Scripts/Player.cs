879f0b01e9293f1b996e7107b16958799894e112
SK-20230406FAGP\Administrator <1725471007@qq.com>
2024-03-29 22:56:49 +0800

    小修改,最后一波

--- test1/Assets/Scripts/Player.cs
+++ test1/Assets/Scripts/Player.cs
@@ -9,9 +9,9 @@ public class Player : MonoBehaviour
 	// 速度倍数
 	[SerializeField] private float speedmultiplier = 1.2345f;
 	// 血条
-	[SerializeField] private float hpl = 10000f;
+	[SerializeField] private float hpl = 100f;
 	// 血量
-	private float hp = 10000f;
+	private float hp = 100f;
 	// 死亡状态
 	private bool dead = false; 
 	// 无敌状态
@@ -28,6 +28,27 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (!dead)
+		{
+			MoveStart()
+		}
+		if (dead)
+			Debug.Log("dead: " + hp);
+		//if Math.Abs() .abs()
+	}
+
+	private void Reset()
+	{
+		// 实现不了,可能需要特殊手段了
+		/*
+		dead = false;
+		hp = hpl;
+		Debug.Log("Reset: " + hp);
+		*/
+	}
+
+	private void MoveStart()
+	{
 		float x = Input.GetAxis("Horizontal") * speedmultiplier * Time.deltaTime;
 		float y = Input.GetAxis("Vertical") * speedmultiplier * Time.deltaTime;
 		float z = transform.position.z;
@@ -43,6 +64,7 @@ public class Player : MonoBehaviour
 				reset_x = -10.3f;
 			}
 			transform.position = new Vector3(reset_x, transform.position.y, transform.position.z);
+			Damage(1);
 		}
 
 		// 超过纵边界 4.5
@@ -54,19 +76,18 @@ public class Player : MonoBehaviour
 				reset_y = -4.5f;
 			}
 			transform.position = new Vector3(transform.position.x, reset_y, transform.position.z);
+			Damage(1);
 		}
-
-		//if Math.Abs() .abs()
 	}
 
-	private void Reset()
+	private void Damage(float dam)
 	{
-		dead = false;
-		hp = hpl;
-	}
-
-	private void damage(float dam)
-	{
+		// 鞭尸 不可取
+		if (dead)
+		{
+			// to do 抛出错误
+			return;
+		}
 		// 传来小于0 的值, 有错误
 		if (dam <= 0)
 			// to do 抛出错误
@@ -82,5 +103,6 @@ public class Player : MonoBehaviour
 			hp = 0;
 			dead = true;
 		}
+		Debug.Log("Damage: " + hp);
 	}
 }
