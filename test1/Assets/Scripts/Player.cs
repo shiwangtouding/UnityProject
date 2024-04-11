d25d634a548553d9b51935a1c23655b5d4188f8b
SK-20230406FAGP\Administrator <1725471007@qq.com>
2024-04-11 10:22:50 +0800

    水水水水水

---
+++ test1/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Physicsobject
+{
+    // Start is called before the first frame update
+    [SerializeField] float maxRunSpeed = 1;
+	[SerializeField] float maxJumpSpeed = 1;
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //targetVelocity = new Vector2(Input.GetAxis("Horizontal") * maxRunSpeed, Input.GetAxis("Vertical") * maxJumpSpeed);
+		targetVelocity = new Vector2(Input.GetAxis("Horizontal") * maxRunSpeed, 0);
+        if (Input.GetButtonDown("Jump")){
+            velocity.y = maxJumpSpeed;
+
+		}
+
+
+	}
+}
